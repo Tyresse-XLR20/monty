ab744acded02531d2afee30faf6eb17d0c84b658
Tyreseb <Tyresemalgas76@gmail.com>
2023-06-23 10:10:45 -0700

    Updated

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing at all
+  *@head: The head of the stack
+  *@counter: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
